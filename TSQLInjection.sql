cdda9044139a0cd22489bcda990b9a7ba6eeeb4e
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2022-08-09 16:02:23 +0300

    Small Changes
    Buna, Din Nou

--- TSQLInjection.sql
+++ TSQLInjection.sql
@@ -1,4 +1,9 @@
 --It checks principals (logins, roles, users, certificates, etc.) having some direct access perm to tables and views.
 SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
-
+GO
+CREATE PROCEDURE TSQLInjection
+AS
+BEGIN --Template
+SELECT 1 
+END --Template
